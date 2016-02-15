5ad86e1edd152719ac9eb44dbc9aab5ad1d7bee3
Junior Samaroo <jns0987654321@gmail.com>
2016-02-14 21:58:40 -0500

    Initial Commit

---
+++ tappity-tap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tappity-tap
+//
+//  Created by Junior Samaroo on 2016-02-14.
+//  Copyright © 2016 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
